4152a659a762057f026a8462b420b4032a560c27
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-06-20 15:38:55 -0400

    Creacion de AppModule.kt

---
+++ app/src/main/java/com/example/tarea4_consumirapiplanets/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.example.tarea4_consumirapiplanets.di
+
+import com.example.tarea4_consumirapiplanets.data.remote.DragonBallApi
+import com.example.tarea4_consumirapiplanets.data.repository.PlanetRepositoryImpl
+import com.example.tarea4_consumirapiplanets.domain.repository.PlanetRepository
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): DragonBallApi {
+        return Retrofit.Builder()
+            .baseUrl("https://dragonball-api.com/api")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DragonBallApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: DragonBallApi): PlanetRepository {
+        return PlanetRepositoryImpl(api)
+    }
+}
