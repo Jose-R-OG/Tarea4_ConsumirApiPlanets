31f24573f5e243c38b1a3947c406dac61eb7f3ff
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-06-28 12:03:57 -0400

    Creacion CharacterRemoteDataSource.kt y CharacterRepositoryImpl.kt para asi completar la capa Data

---
+++ app/src/main/java/com/example/tarea4_consumirapiplanets/data/remote/remotedatasource/CharacterRemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.tarea4_consumirapiplanets.data.remote.remotedatasource
+
+import com.example.tarea4_consumirapiplanets.data.remote.DragonBallApi
+import com.example.tarea4_consumirapiplanets.data.remote.dtos.CharacterDto
+import com.example.tarea4_consumirapiplanets.data.remote.dtos.CharacterResponseDto
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class CharacterRemoteDataSource @Inject constructor(private val api: DragonBallApi) {
+    suspend fun getCharacters(page: Int, limit: Int, name: String?, gender: String?, race: String?): Result<CharacterResponseDto> {
+        return try {
+            val response = api.getCharacters(page, limit, name, gender, race)
+            if (response.isSuccessful) Result.success(response.body()!!) else Result.failure(Exception("Error de red"))
+        } catch (e: Exception) { Result.failure(e) }
+    }
+
+    suspend fun getCharacterDetail(id: Int): Result<CharacterDto> {
+        return try {
+            val response = api.getCharacterDetail(id)
+            if (response.isSuccessful) Result.success(response.body()!!) else Result.failure(Exception("Error de red"))
+        } catch (e: Exception) { Result.failure(e) }
+    }
+}
