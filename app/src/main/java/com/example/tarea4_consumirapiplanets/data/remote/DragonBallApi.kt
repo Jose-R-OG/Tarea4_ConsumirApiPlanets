35254e57227c33a00a296293a85b1b723cb42fd0
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-06-28 11:57:37 -0400

    Modificacion de DragonBallApi.kt para agregar endpoints de character y creacion de CharacterDto.kt

--- app/src/main/java/com/example/tarea4_consumirapiplanets/data/remote/DragonBallApi.kt
+++ app/src/main/java/com/example/tarea4_consumirapiplanets/data/remote/DragonBallApi.kt
@@ -1,5 +1,7 @@
 package com.example.tarea4_consumirapiplanets.data.remote
 
+import com.example.tarea4_consumirapiplanets.data.remote.dtos.CharacterDto
+import com.example.tarea4_consumirapiplanets.data.remote.dtos.CharacterResponseDto
 import com.example.tarea4_consumirapiplanets.data.remote.dtos.PlanetDto
 import com.example.tarea4_consumirapiplanets.data.remote.dtos.PlanetResponseDto
 import retrofit2.Response
@@ -24,4 +26,18 @@ interface DragonBallApi {
     suspend fun getPlanetDetail(
         @Path("id") id: Int
     ): Response<PlanetDto>
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?,
+        @Query("gender") gender: String?,
+        @Query("race") race: String?
+    ): Response<CharacterResponseDto>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterDetail(
+        @Path("id") id: Int
+    ): Response<CharacterDto>
 }
